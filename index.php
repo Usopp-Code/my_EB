b05d1550e34b7ce832f0182477755866c2b28bae
leolee_1992 <leolee_1992@163.com>
2016-06-15 17:06:46 +0800

    f

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
-<?php
-echo "sss";
+<html>
+<body>
+<h2 align='center' color='red'>欢迎来到李二狗的狗窝！</h2>
+<p style="text-align:center">
+<img src="mylogo.png" alt='myweixin' align='middle' />
+</p>
+</html>
